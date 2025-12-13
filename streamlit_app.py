2265d0521acd1e7c8683a96a4187d8f03d96bcf2
srouchier <s.rouchier@gmail.com>
2025-12-13 14:12:36 +0100

    attempt cmdstan install

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,15 @@ pg = st.navigation([st.Page("0_index.py", title="Welcome", icon="🏠"),
 with st.sidebar:
     st.write("I will probably use this sidebar to list trained models so the user can load and compare them. Models trained in this session, or loaded from a previous session.")
 
-#if "cmdstan_installed" not in st.session_state:
-#    cmdstanpy.install_cmdstan()
-#    st.session_state["cmdstan_installed"] = True
+# Attempt install
+if "cmdstan_installed" not in st.session_state:
+    try:
+        with st.spinner("Checking/Installing CmdStan..."):
+            cmdstanpy.install_cmdstan()
+        st.session_state["cmdstan_installed"] = True
+    except Exception as e:
+        st.session_state["cmdstan_installed"] = False
+        st.warning("CmdStan install failed")
+        st.exception(e)
 
 pg.run()
